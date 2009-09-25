1d739d1bfc7b544382066ebf9c6df7895c95cd60
Nick Mathewson <nickm@torproject.org>
2009-09-25 01:18:38 -0400

    Implement a sqlite replacement for our current db wrappers.
    Now all we'll need to do is reverse-engineer our current DB usage,
    design a schema, write a migration tool, and switch the code to use
    sqlite.
    
    Such fun!

--- lib/bridgedb/Tests.py
+++ lib/bridgedb/Tests.py
@@ -3,21 +3,21 @@
 # See LICENSE for licensing information
 
 import doctest
+import os
+import random
+import sqlite3
+import tempfile
 import unittest
 import warnings
-import random
 
 import bridgedb.Bridges
 import bridgedb.Main
 import bridgedb.Dist
 import bridgedb.Time
+import bridgedb.Storage
 
 def suppressWarnings():
     warnings.filterwarnings('ignore', '.*tmpnam.*')
-
-class TestCase0(unittest.TestCase):
-    def testFooIsFooish(self):
-        self.assert_(True)
 
 def randomIP():
     return ".".join([str(random.randrange(1,256)) for _ in xrange(4)])
@@ -82,11 +82,57 @@ class IPBridgeDistTests(unittest.TestCase):
             self.assertEquals(len(fps), 5)
             self.assertTrue(count >= 1)
 
+class StorageTests(unittest.TestCase):
+    def setUp(self):
+        self.fd, self.fname = tempfile.mkstemp()
+        self.conn = sqlite3.Connection(self.fname)
+
+    def tearDown(self):
+        self.conn.close()
+        os.close(self.fd)
+        os.unlink(self.fname)
+
+    def testSimpleDict(self):
+        self.conn.execute("CREATE TABLE A ( X PRIMARY KEY, Y )")
+        d = bridgedb.Storage.SqliteDict(self.conn, self.conn.cursor(),
+                                        "A", (), (), "X", "Y")
+
+        self.basictests(d)
+
+    def testComplexDict(self):
+        self.conn.execute("CREATE TABLE B ( X, Y, Z, "
+                          "CONSTRAINT B_PK PRIMARY KEY (X,Y) )")
+        d = bridgedb.Storage.SqliteDict(self.conn, self.conn.cursor(),
+                                        "B", ("X",), ("x1",), "Y", "Z")
+        d2 = bridgedb.Storage.SqliteDict(self.conn, self.conn.cursor(),
+                                         "B", ("X",), ("x2",), "Y", "Z")
+        self.basictests(d)
+        self.basictests(d2)
+
+    def basictests(self, d):
+        d["hello"] = "goodbye"
+        d["hola"] = "adios"
+        self.assertEquals(d["hola"], "adios")
+        d["hola"] = "hasta luego"
+        self.assertEquals(d["hola"], "hasta luego")
+        self.assertEquals(sorted(d.keys()), [u"hello", u"hola"])
+        self.assertRaises(KeyError, d.__getitem__, "buongiorno")
+        self.assertEquals(d.get("buongiorno", "ciao"), "ciao")
+        self.conn.commit()
+        d["buongiorno"] = "ciao"
+        del d['hola']
+        self.assertRaises(KeyError, d.__getitem__, "hola")
+        self.conn.rollback()
+        self.assertEquals(d["hola"], "hasta luego")
+        self.assertEquals(d.setdefault("hola","bye"), "hasta luego")
+        self.assertEquals(d.setdefault("yo","bye"), "bye")
+        self.assertEquals(d['yo'], "bye")
+
 def testSuite():
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
 
-    for klass in [ TestCase0, IPBridgeDistTests ]:
+    for klass in [ IPBridgeDistTests, StorageTests ]:
         suite.addTest(loader.loadTestsFromTestCase(klass))
 
     for module in [ bridgedb.Bridges,
@@ -99,7 +145,7 @@ def testSuite():
 
 def main():
     suppressWarnings()
-    
+
     unittest.TextTestRunner(verbosity=1).run(testSuite())
 
 
