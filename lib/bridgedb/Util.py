0fc78766298a1f63cc6487baadc2a1b76eb3a45b
Isis Lovecruft <isis@torproject.org>
2013-07-17 02:22:03 +0000

    Add docstrings to Util.logSafely and Util.set_safe_logging.
     * FIX two more mixed print/strfmt syntax formatted strings being passed to
       the logger.
     * ADD file header to lib/bridgedb/Util.py.
    
    Conflicts:
    	lib/bridgedb/Dist.py
    	lib/bridgedb/EmailServer.py
    	lib/bridgedb/HTTPServer.py
    	lib/bridgedb/Main.py

--- lib/bridgedb/Util.py
+++ lib/bridgedb/Util.py
@@ -1,12 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of BridgeDB, a Tor bridge distribution system.
+#
+# :authors: please also see AUTHORS file
+# :copyright: (c) 2007-2013, The Tor Project, Inc.
+#             (c) 2007-2013, all entities within the AUTHORS file
+# :license: 3-Clause BSD, see LICENSE for licensing information
+
+
+#: If True, scrub IP and email addresses from logs.
 safe_logging = True
 
-def set_safe_logging(safe):
+def set_safe_logging(safe=True):
+    """Enable or disable scrubbing of information in the logs.
+
+    :param safe: If ``True``, use :meth:`bridgedb.log.safe` to scrub logs.
+    """
     global safe_logging
     safe_logging = safe
 
 def logSafely(val):
+    """Scrub a string or return the original string.
+
+    :rtype: str
+    :returns: '[scrubbed]' if SAFE_LOGGING is enabled, else ``val``.
+    """
     if safe_logging:
         return "[scrubbed]"
-    else:
-        return val
-
+    return val
