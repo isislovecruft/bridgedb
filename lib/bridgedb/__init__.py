a63422cfff9b707fb2f9227f35be5cdd85a75d17
Isis Lovecruft <isis@torproject.org>
2013-10-15 03:35:34 +0000

    Add a real fileheader and full version string to bridgedb/__init__.py.
    Might as well, while we're at it.

--- lib/bridgedb/__init__.py
+++ lib/bridgedb/__init__.py
@@ -1,10 +1,17 @@
-
-# This file tells Python that this is an honest to goodness package.
+#
+# This file is part of BridgeDB, a Tor bridge distribution system.
+#
+# :authors: Isis Lovecruft 0xA3ADB67A2CDB8B35 <isis@torproject.org>
+#           please also see AUTHORS file
+# :copyright: (c) 2007-2013, The Tor Project, Inc.
+#             (c) 2007-2013, all entities within the AUTHORS file
+# :license: 3-clause BSD, see included LICENSE for information
 
 from ._version import get_versions
 from ._langs import get_langs
 
 __version__ = get_versions()['version']
+__version_full__ = get_versions()['full']
 __langs__ = get_langs()
 
 del get_versions
