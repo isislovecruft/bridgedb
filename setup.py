948ee1b5ec9ca71628ddd3e981d9897c9e44b066
Isis Lovecruft <isis@torproject.org>
2013-10-15 14:48:32 +0000

    Fix error on Python2.6 resulting from Python3.1 indexless format string syntax.

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ def get_supported_langs():
     with open(repo_langs, 'r') as langsfile:
         for line in langsfile.readlines():
             if line.startswith('supported'):
-                line = "supported = {}\n".format(supported)
+                line = "supported = %s\n" % supported
                 print("REWROTE supported langs: %s" % line)
             new_langs_lines.append(line)
     with open(repo_langs, 'w') as newlangsfile:
