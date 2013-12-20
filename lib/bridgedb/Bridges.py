6ba48b9a02f6e3dc1d3724ee01e51a19a3853e5b
Isis Lovecruft <isis@torproject.org>
2013-12-20 06:58:33 +0000

    Fix doctest context in bridgedb.Bridges.is_valid_ip().

--- lib/bridgedb/Bridges.py
+++ lib/bridgedb/Bridges.py
@@ -35,17 +35,18 @@ def is_valid_ip(ip):
     """Return True if ip is the string encoding of a valid IPv4 address,
        and False otherwise.
 
-    >>> is_valid_ip('1.2.3.4')
+    >>> from bridgedb import Bridges
+    >>> Bridges.is_valid_ip('1.2.3.4')
     True
-    >>> is_valid_ip('1.2.3.255')
+    >>> Bridges.is_valid_ip('1.2.3.255')
     True
-    >>> is_valid_ip('1.2.3.256')
+    >>> Bridges.is_valid_ip('1.2.3.256')
     False
-    >>> is_valid_ip('1')
+    >>> Bridges.is_valid_ip('1')
     False
-    >>> is_valid_ip('1.2.3')
+    >>> Bridges.is_valid_ip('1.2.3')
     False
-    >>> is_valid_ip('xyzzy')
+    >>> Bridges.is_valid_ip('xyzzy')
     False
     """
 
