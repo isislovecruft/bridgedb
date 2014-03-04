019859f2d0f7b2edcfe58cbdc5512cf3348f4dcb
Isis Lovecruft <isis@torproject.org>
2014-03-04 04:32:06 +0000

    Update copyright year in lib/bridgedb/test/test_parse_networkstatus.py.

--- lib/bridgedb/test/test_parse_networkstatus.py
+++ lib/bridgedb/test/test_parse_networkstatus.py
@@ -3,10 +3,8 @@
 # This file is part of BridgeDB, a Tor bridge distribution system.
 #
 # :authors: Isis Lovecruft 0xA3ADB67A2CDB8B35 <isis@torproject.org>
-#           please also see AUTHORS file
-# :copyright: (c) 2013, Isis Lovecruft
-#             (c) 2007-2013, The Tor Project, Inc.
-#             (c) 2007-2013, all entities within the AUTHORS file
+# :copyright: (c) 2013-2014, Isis Lovecruft
+#             (c) 2007-2014, The Tor Project, Inc.
 # :license: 3-Clause BSD, see LICENSE for licensing information
 
 """Unittests for the :mod:`bridgedb.parse.networkstatus` module.
