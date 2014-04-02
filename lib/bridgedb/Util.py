cc2aab80c9b955ab888b8ee7e7ecae72bd1bd713
Isis Lovecruft <isis@torproject.org>
2014-04-02 14:16:18 +0000

    Add file headers and module docstring to bridgedb.Util.

--- lib/bridgedb/Util.py
+++ lib/bridgedb/Util.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 ; test-case-name: bridgedb.test.test_Util -*-
+#
+# This file is part of BridgeDB, a Tor bridge distribution system.
+#
+# :authors: Isis Lovecruft 0xA3ADB67A2CDB8B35 <isis@torproject.org>
+#           Matthew Finkel 0x017DD169EA793BE2 <sysrqb@torproject.org>
+# :copyright: (c) 2013-2014, Isis Lovecruft
+#             (c) 2013-2014, Matthew Finkel
+#             (c) 2007-2014, The Tor Project, Inc.
+# :license: 3-Clause BSD, see LICENSE for licensing information
+
+"""Common utilities for BridgeDB."""
+
 safe_logging = True
 
 def set_safe_logging(safe):
