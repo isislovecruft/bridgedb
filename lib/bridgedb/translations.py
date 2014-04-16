ed438d0c2b00f02c29b60e561d37f241f6a6a4b8
Isis Lovecruft <isis@torproject.org>
2014-04-16 16:22:40 +0000

    Move locate determination and translation code into separate module.
     * ADD new bridgedb.translations module with the following methods:
    
       - getFirstSupportedLang():
         Essentially the same as the original HTTPServer.getAssumedChosenLang()
         function, except that the determination of which languages are
         supported uses _lang.getLangs() instead (this has been in the _lang
         module for a long time, and was meant for that purpose).
    
       - getLocaleFromHTTPRequest():
         This is taken mostly from the original setLocaleFromRequestHeader()
         (in the HTTPServer module) except that there was an extra function in
         the EmailServer module, called getLocaleFromRequest() ― which was not
         in use anywhere ― which included the parsing of '?lang=' HTTP GET/POST
         arguments. The parsing of '?lang=' was taken from that function and
         added to the original functionality of
         setLocaleFromRequestHeader(). This fixes part of ticket #9678:
         "'Select Language' button on bridges.tpo".
    
      - getLocaleFromPlusAddr():
        Exactly the same as the original in the EmailServer module.
    
      - installTranslations():
        Produces a gettext translation object, with fallback languages chained
        to it in order of client preference.
    
      - usingRTLLang():
        Essentially the same as the original in the HTTPServer module, except
        that the parsing of supported langs again uses the _lang.getLangs()
        function, as above.
    
     * FIXES part of #9678: "'Select Language' button to bridges.tpo"

---
+++ lib/bridgedb/translations.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 ; test-case-name: bridgedb.test.test_translations -*-
+#
+# This file is part of BridgeDB, a Tor bridge distribution system.
+#
+# :authors: Isis Lovecruft 0xA3ADB67A2CDB8B35 <isis@torproject.org>
+# :copyright: (c) 2013-2014, Isis Lovecruft
+#             (c) 2007-2014, The Tor Project, Inc.
+# :license: 3-Clause BSD, see LICENSE for licensing information
+
+import gettext
+import logging
+import os
+import re
+
+from bridgedb import _langs
+from bridgedb import safelog
+from bridgedb.parse import headers
+
+
+TRANSLATIONS_DIR = os.path.join(os.path.dirname(__file__), 'i18n')
+
+
+def getFirstSupportedLang(langs):
+    """Return the first language in **langs** that we support.
+
+    :param list langs: All requested languages
+    :rtype: str
+    :returns: A country code for the client's preferred language.
+    """
+    lang = 'en-US'
+    supported = _langs.get_langs()
+
+    for l in langs:
+        if l in supported:
+            lang = l
+            break
+    return lang
+
+def getLocaleFromHTTPRequest(request):
+    """Retrieve the languages from an HTTP ``Accept-Language:`` header.
+
+    Parse the languages from the header, use them to install a
+    ``gettext.translation`` chain via :func:`installTranslations`, and lastly
+    return the requested languages.
+
+    :type request: :api:`twisted.web.server.Request`
+    :param request: An incoming request from a client.
+    :rtype: list
+    :returns: All requested languages.
+    """
+    header = request.getHeader('accept-language')
+    if header is None:
+        logging.debug("Client sent no 'Accept-Language' header. Using fallback.")
+        header = 'en,en-US'
+
+    langs = headers.parseAcceptLanguage(header)
+    if not safelog.safe_logging:  # pragma: no cover
+        logging.debug("Client Accept-Language (top 5): %s" % langs[:5])
+
+    # Check if we got a ?lang=foo argument, and if we did, insert it first
+    chosenLang = request.args.get("lang", [None,])[0]
+    if chosenLang:
+        logging.debug("Client requested language: %r" % chosenLang)
+        langs.insert(0, chosenLang)
+
+    installTranslations(langs)
+    return langs
+
+def getLocaleFromPlusAddr(address):
+    """See whether the user sent his email to a 'plus' address, for instance to
+    bridges+fa@bridges.torproject.org. Plus addresses are the current
+    mechanism to set the reply language.
+    """
+    replyLocale = "en"
+    r = '.*(<)?(\w+\+(\w+)@\w+(?:\.\w+)+)(?(1)>)'
+    match = re.match(r, address)
+    if match:
+        replyLocale = match.group(3)
+
+    return replyLocale
+
+def installTranslations(langs):
+    """Create a ``gettext.translation`` chain for all **langs**.
+
+    Attempt to install the first language in the **langs** list. If that
+    fails, we receive a ``gettext.NullTranslation`` object, and if it worked
+    then we have a ``gettext.GNUTranslation`` object. Whichever one we end up
+    with, get the other languages and add them as fallbacks to the
+    first. Lastly, install this chain of translations.
+
+    :param list langs: A list of language codes.
+    :returns: A ``gettext.NullTranslation`` or ``gettext.GNUTranslation`` with
+        fallback languages set.
+    """
+    try:
+        language = gettext.translation("bridgedb", localedir=TRANSLATIONS_DIR,
+                                       languages=langs, fallback=True)
+        for lang in langs:
+            language.add_fallback(
+                gettext.translation("bridgedb", localedir=TRANSLATIONS_DIR,
+                                    languages=langs, fallback=True))
+    except IOError as error:
+        logging.error(error.message)
+
+    language.install(unicode=True)
+    return language
+
+def usingRTLLang(langs):
+    """Check if we should translate the text into a RTL language.
+
+    Choose the first language from the **langs** list that we support and
+    return True if it is a RTL language, else return False.
+
+    :param list langs: An incoming request.
+    :rtype: bool
+    :returns: ``True`` if the preferred language is right-to-left; ``False``
+        otherwise.
+    """
+    lang = getFirstSupportedLang(langs)
+    if lang in _langs.RTL_LANGS:
+        return True
+    return False
