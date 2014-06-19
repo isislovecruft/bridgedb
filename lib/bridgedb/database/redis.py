30230f6ee836a33d5fc87b8c82eef933b9f7c14b
Isis Lovecruft <isis@torproject.org>
2014-06-19 02:33:27 +0000

    Second implementation of Redis backend to strore NS descriptors.
    There are actually two implementations here:
    
     1) Creates a RedisClient for each transaction. This is logistically and
         conceptually simpler.
    
     2) Creates a single RedisClient, then creates a DeferredList which
         essentially pipelines transactions over a single database connection to
         feed descriptors into the database as fast as the database can handle it.
    
    It turns out the #1 usually results in OS errors due to ulimit, resultant from
    opening too many file descriptors simultaneously. Particularly, it becomes
    reproduceable once there are at least 1000 descriptors.
    
    Implementation #2 works much better in practice, and so it is worth the extra
    complexity. Plus, it averages roughly 5 times faster than #1 (with 250
    descriptors: #1 takes an average of 62ms; #2 takes 12ms).

--- lib/bridgedb/database/redis.py
+++ lib/bridgedb/database/redis.py
@@ -20,6 +20,10 @@ from bridgedb import safelog
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 
+#: The number of seconds to expire strored bridge networkstatus documents
+#: after. (default: 604800, i.e. 1 week)
+DESC_EXPIRE = 7 * 24 * 60 * 60
+
 #: This will be our global ``txredis.client.RedisClientFactory``, created with
 #: :func:`buildClientFactory`.
 theFactory = None
@@ -102,8 +106,7 @@ def connectServer(host=REDIS_HOST, port=REDIS_PORT, password=None, **kwargs):
         supplied), such that it is ready to interact with the server and begin
         issuing commands.
     """
-    # This is really noisey:
-    #logging.debug("REDIS: Connecting to server: %s:%d" % (host, port))
+    logging.debug("REDIS: Connecting to server: %s:%d" % (host, port))
 
     def cb(result):
         logging.debug("REDIS: Connection successful: %s" % hash(result))
@@ -118,45 +121,125 @@ def connectServer(host=REDIS_HOST, port=REDIS_PORT, password=None, **kwargs):
     return redis
 
 def getNetworkStatusKey(fingerprint):
-    return fingerprint + u'_ns'
+    """Create a key for storing a networkstatus descriptor in Redis, based upon
+    the router's **fingerprint**.
 
-#: The number of seconds to expire strored bridge networkstatus documents
-#: after. (default: 604800, i.e. 1 week)
-DESC_EXPIRE = 7 * 24 * 60 * 60
+    :param str fingerprint: The router's identity fingerprint.
+    :rtype: str
+    :returns: The **fingerprint** with ``'_ns'`` appended to it. If for some
+        reason, the fingerprint is ``None`` (this seems to happen sometimes),
+        then ``None`` is returned.
+    """
+    if fingerprint:
+        return u"%s_ns" % fingerprint
 
 def setNetworkStatuses(routers, **kwargs):
-    """DOCDOC
+    """Given a bunch of parsed networkstatus documents, create a single
+    ``RedisClient`` which pipelines the handling of atomic database
+    transactions for storing each document by it corresponding router
+    fingerprint.
 
     :param dict routers: A mapping of [bridge-]router fingerprints to
         :api:`~stem.descriptor.router_status_entry._RouterStatusEntryV2`
         instances, as returned from from the ``routers`` attribute of a
         :api:`~stem.descriptor.networkstatus.BridgeNetworkStatusDocument`.
+    :kwargs: are passed to :func:`connectServer`, which creates the
+        ``RedisClient``.
     """
     logging.info(("REDIS: Attempting to store %d bridge networkstatus "
                   "documents.") % (len(routers.keys())))
-    redis = connectServer(**kwargs)
 
-    def setDesc(db, router):
+    def setDescriptor(redis, router):
+        """Store the networkstatus descriptor in Redis using SETEX.
+
+        :type redis: ``txredis.client.RedisClient``
+        :param redis: A ``RedisClient`` connected to a Redis server, i.e. the
+            result of the ``Deferred`` returned from :func:`connectServer`.
+        :type router:
+            :api:`~stem.descriptor.router_status_entry.RouterStatusEntryV2`
+        :param router: A single parsed networkstatus document (for a single
+            router) from Stem.
+        """
         fingerprint, descriptor = router
+        # We don't want to serialise the entire file that the single
+        # networkstatus document originated from, so we need to override this
+        # attribute with just the single descriptor before serialisation:
+        descriptor.document = str(descriptor)
+        descriptorLength = len(str(descriptor))
+        if descriptorLength >= 1000:
+            logging.warn("Got huge networkstatus descriptor with length=%d: %s"
+                         % (descriptorLength, str(descriptor)))
+
         key = getNetworkStatusKey(fingerprint)
         jellied = jelly.jelly(descriptor)
         logging.debug("REDIS: SETEX %s ..." % (key))
-        response = db.set(key, jellied, expire=DESC_EXPIRE)
-        return response
+        response = redis.set(key, jellied, expire=DESC_EXPIRE)
+        result = (redis, key, response)
+        return result
 
-    def setDescEB(failure):
-        tb = failure.getTraceback()
-        logging.error(tb)
+    def setDescriptorEB(fail):
+        logging.error(fail.getTraceback())
+        return fail
 
-    responses = []
+    def transactionsResults(results):
+        """Count the successes and handle the failures in the list of results
+        returned from the :api:`~twisted.internet.defer.DeferredList` of Redis
+        transactions.
+
+        .. info: This function send the ``QUIT`` command to the
+            ``RedisClient``.
+
+        :param list results: A list of 2-tuples. The first value in each tuple
+            is a boolean, representing whether the transaction was
+            successful. The second is the result value for the underlying
+            deferred.
+        :rtype: :api:`~twisted.internet.defer.Deferred`
+        :returns: A deferred which will eventually fire back with the result
+            of the final ``QUIT`` command for the ``RedisClient``.
+        """
+        okay = 0
+        fails = []
+        redis = results[0][1][0]
+
+        # The `success` here is a boolean, while the `value` is the returned
+        # result of :func:`setDescriptor`, i.e.::
+        #   (<RedisClient at 0x1234>, key, <Deferred at 0x5678>)
+        for (success, value) in results:
+            if success:
+                okay += 1
+                logging.debug("REDIS: %s: OK" % value[1])
+            else:
+                fails.append(value)
+
+        logging.info("REDIS: %d success, %d failed" % (okay, len(fails)))
+        [logging.warn("REDIS: %r" % repr(fail)) for fail in fails]
+        logging.debug("REDIS: QUIT %s" % hash(redis))
+        d = redis.quit()
+        return d
+
+    def redisCB(redis):
+        """Callback every waiting database transaction in the ``DeferredList``
+        with the ready ``RedisClient``, when the later has finished connecting
+        to the Redis server.
+
+        :type redis: ``txredis.client.RedisClient``
+        :param redis: A ``RedisClient`` connected to a Redis server, i.e. the
+            result of the ``Deferred`` returned from :func:`connectServer`.
+        """
+        [d.callback(redis) for d in transactions]
+        return redis
+
+    transactions = []
     for router in routers.items():
-        d1 = connectServer(**kwargs)
-        d1.addErrback(setDescEB)
-        d2 = defer.Deferred()
-        d2.addCallback(setDesc, router)
-        d2.addErrback(setDescEB)
-        d1.chainDeferred(d2)
+        d = defer.Deferred()
+        d.addCallback(setDescriptor, router)
+        d.addCallbacks(defer.passthru, setDescriptorEB)
+        transactions.append(d)
 
+    redis = connectServer(**kwargs)
+    dl = defer.DeferredList(transactions, consumeErrors=True)
+    dl.addCallback(transactionsResults)
+    redis.addCallback(redisCB)
     return redis
 
 def storeNetworkStatus(router, **kwargs):
@@ -178,29 +261,23 @@ def storeNetworkStatus(router, **kwargs):
 
         logging.debug("REDIS: SETEX %s ..." % (key))
         response = db.set(key, jellied, expire=DESC_EXPIRE)
-        result = (db, response)
+        result = (db, key, response)
 
         return result
 
     def storeDescriptorCB(result):
-        (db, response) = result
+        (db, key, response) = result
 
-        def logResponse(response):
-            logging.debug("REDIS: server response: %s" % response)
-        response.addCallbacks(logResponse, logging.error)
+        def logResponse(response, key):
+            logging.debug("REDIS: %s: %s" % (key, response))
+        response.addCallback(logResponse, key)
 
         logging.debug("REDIS: QUIT %s" % hash(db))
         d = db.quit()
-        d.addCallbacks(logResponse, logging.error)
         return d
 
     def storeDescriptorEB(fail):
         logging.error(fail.getTraceback())
-
-    def killConnection(db):
-        logging.debug("REDIS: QUIT %s" % db)
-        db.quit()
-        return db
 
     d1 = connectServer(**kwargs)
     d2 = defer.Deferred()
@@ -217,11 +294,22 @@ if __name__ == "__main__":
 
     password = "bridgedbtestingbridgedbtestingbridgedbtesting"
     rundir = '/home/isis/code/torproject/bridgedb/run-manual'
-    filename = "%s/10000_networkstatus-bridges" % rundir
+    filename = "%s/250_networkstatus-bridges" % rundir
     bridgeNetworkStatusDocument = descriptors.parseNetworkStatusFile(filename)
     bridgeRouters = bridgeNetworkStatusDocument.routers
 
-    for router in bridgeRouters.items():
-        storeNetworkStatus(router, password=password)
+    #     This version creates a client for every single transaction. For 250
+    # networkstatus descriptors, the average runtime is 62ms.
 
+    #for router in bridgeRouters.items():
+    #    storeNetworkStatus(router, password=password)
+
+    #     This version creates a single client, and creates a DeferredList,
+    # which acts as a queue, feeding transactions onto the wire as fast as they
+    # can be handled. For 250 networkstatus descriptors, the average runtime is
+    # 20ms.
+
+    setNetworkStatuses(bridgeRouters, password=password)
+
+    reactor.callLater(2, reactor.stop)
     reactor.run()
