e10e20027f74cb75a2b989910c61bab03e1a17bd
Isis Lovecruft <isis@torproject.org>
2014-07-06 19:17:15 +0000

    Add file header to test_parse_descriptors.py.

--- lib/bridgedb/test/test_parse_descriptors.py
+++ lib/bridgedb/test/test_parse_descriptors.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of BridgeDB, a Tor bridge distribution system.
+#
+# :authors: Isis Lovecruft 0xA3ADB67A2CDB8B35 <isis@torproject.org>
+# :copyright: (c) 2013-2014, Isis Lovecruft
+#             (c) 2007-2014, The Tor Project, Inc.
+# :license: 3-Clause BSD, see LICENSE for licensing information
 
 """Unittests for :class:`bridgedb.parse.descriptors` module."""
 
