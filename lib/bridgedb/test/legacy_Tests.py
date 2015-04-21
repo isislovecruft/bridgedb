cf70905fd3822813c0cccdf7aa9586d74a37aa06
Isis Lovecruft <isis@torproject.org>
2015-04-21 23:34:50 +0000

    Rewrite testDistWithFilterIP4() in legacy_Tests.

--- lib/bridgedb/test/legacy_Tests.py
+++ lib/bridgedb/test/legacy_Tests.py
@@ -252,22 +252,6 @@ class IPBridgeDistTests(unittest.TestCase):
             assert type(ipaddr.IPAddress(address)) is ipaddr.IPv6Address
             assert filterBridgesByIP6(random.choice(bridges))
 
-    def testDistWithFilterIP4(self):
-        d = bridgedb.Dist.HTTPSDistributor(3, "Foo")
-        for _ in xrange(250):
-            d.insert(fakeBridge6(or_addresses=True))
-            d.insert(fakeBridge(or_addresses=True))
-
-        for i in xrange(500):
-            bridges = d.getBridges(randomIPv4String(),
-                                   "faketimestamp",
-                                   bridgeFilterRules=[filterBridgesByIP4])
-            bridge = random.choice(bridges)
-            bridge_line = bridge.getConfigLine(addressClass=ipaddr.IPv4Address)
-            address, portlist = networkstatus.parseALine(bridge_line)
-            assert type(ipaddr.IPAddress(address)) is ipaddr.IPv4Address
-            assert filterBridgesByIP4(random.choice(bridges))
-
     def testDistWithFilterBlockedCountriesAdvanced(self):
         d = bridgedb.Dist.HTTPSDistributor(3, "Foo")
         for _ in xrange(250):
