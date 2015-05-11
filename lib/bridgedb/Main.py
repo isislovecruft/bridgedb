3bad1b9fcb698ba5d74a07499dcdd23409d5b044
Isis Lovecruft <isis@torproject.org>
2015-05-11 21:24:07 +0000

    Remove unnecessary database initialisation from bridgedb.Main.
    Conflicts:
    	lib/bridgedb/Main.py

--- lib/bridgedb/Main.py
+++ lib/bridgedb/Main.py
@@ -378,7 +378,6 @@ def run(options, reactor=reactor):
 
         # Initialize our DB.
         bridgedb.Storage.initializeDBLock()
-        db = bridgedb.Storage.openDatabase(cfg.DB_FILE + ".sqlite")
         bridgedb.Storage.setDBFilename(cfg.DB_FILE + ".sqlite")
         load(state, splitter, clear=False)
 
