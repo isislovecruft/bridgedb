c5cce356723a525050f6780ca87e7e2954bacc31
Isis Lovecruft <isis@torproject.org>
2015-05-12 07:28:26 +0000

    PEP8 remove extra blank line in bridgedb.bridges module.

--- lib/bridgedb/bridges.py
+++ lib/bridgedb/bridges.py
@@ -1084,7 +1084,6 @@ class Bridge(BridgeBackwardsCompatibility):
                 ("Client requested transport %s, but bridge %s doesn't "
                 "have any of that transport!") % (desired, self))
 
-
         unblocked = []
         for pt in transports:
             if not sum([self.transportIsBlockedIn(cc, pt.methodname)
