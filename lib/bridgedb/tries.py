55faf5d56e0dd12b2640e89944aa817bbf97667f
Isis Lovecruft <isis@torproject.org>
2015-06-05 10:25:06 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add new bridgedb.tries module for hashring branch structures.

--- lib/bridgedb/tries.py
+++ lib/bridgedb/tries.py
@@ -18,92 +18,10 @@ import logging
 from functools import wraps
 from Queue import Queue
 
-import sexpdata as sexp
+from bridgedb.parse.sexp import fromSexp
+from bridgedb.parse.sexp import isSexp
+from bridgedb.parse.sexp import toSexp
 
-
-class SexpressionError(TypeError):
-    """Something couldn't be serialized or deserialized as an s-expression."""
-
-
-def isSexp(exp):
-    """Determine if **exp** is a valid s-expression.
-
-    :param str exp: An s-expression.
-    :rtype: bool
-    :returns: ``True`` if **exp** is a valid s-expression; ``False``
-        otherwise.
-    """
-    try:
-        sexp.loads(exp)
-    except (TypeError, AssertionError):
-        pass
-    else:
-        return True
-    return False
-
-def fromSexp(exp):
-    """Deserialize a potential s-expression into a list.
-
-    >>> from __future__ import unicode_literals
-    >>> from bridgedb.tries import fromSexp
-    >>> fromSexp([1, 2, 3])
-    [1, 2, 3]
-    >>> fromSexp('(1 2 3)')
-    [1, 2, 3]
-    >>> fromSexp([3, [1, [4, [1]]]])
-    [3, [1, [4, [1]]]]
-    >>> fromSexp(["a", "b", "c"])
-    ["a", "b", "c"]
-
-    :type exp: str or list or tuple
-    :param exp: A potential s-expression.
-    :raises SexpressionError: if **exp** is not a type that can be
-        deserialized.
-    :rtype: list
-    :returns: A Python list representation of the original **sexp**.
-    """
-    if isinstance(exp, (list, tuple)):
-        exp = toSexp(exp)
-    if isinstance(exp, basestring):
-        try:
-            return sexp.loads(exp)
-        except (TypeError, AssertionError):
-            raise SexpressionError(
-                ("Cannot deserialize %r as an s-expression: "
-                 "Expected str; got %s.") % (exp, type(exp)))
-    else:
-        raise SexpressionError(
-            ("Cannot deserialize %r as an s-expression: "
-             "Expected str; got %s.") % (exp, type(exp)))
-
-def toSexp(exp):
-    """Serialize **exp** into a s-expression.
-
-    >>> from bridgedb.tries import toSexp
-    >>> toSexp([1, 2, 3])
-    u'(1 2 3)'
-    >>> toSexp('(1 2 3)')
-    u'(1 2 3)'
-    >>> toSexp([3, [1, [4, [1]]]])
-    u'(3 (1 (4 (1))))'
-    >>> toSexp(["a", "b", "c"])
-    u'("a" "b" "c")'
-
-    :type exp: str or list or tuple
-    :param exp: The expression to convert to an s-expression.
-    :raises SexpressionError: if **exp** cannot be parsed into an
-        s-expression.
-    :rtype: str
-    :returns: The **exp**, as an s-expression.
-    """
-    if isSexp(exp):
-        return type('')(exp)
-    elif isinstance(exp, (list, tuple)):
-        return sexp.dumps(exp)
-    else:
-        raise SexpressionError(
-            ("Cannot serialize %r as an s-expression: "
-             "Expected list or tuple; got %s.") % (exp, type(exp)))
 
 def tooCloseToTrunk(key, node):
     """If the number of matching elements in the **key** is
