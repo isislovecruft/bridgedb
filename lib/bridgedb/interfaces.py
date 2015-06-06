27cac3812956dbf999f0b0c111150b40fadc7525
Isis Lovecruft <isis@torproject.org>
2015-06-06 00:49:21 +0000

    fixup! Add bridgedb.distribute module with IDistribute and Distributor classes.

--- lib/bridgedb/interfaces.py
+++ lib/bridgedb/interfaces.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 ; test-case-name: bridgedb.test.test_interfaces ; -*-
 #_____________________________________________________________________________
 #
 # This file is part of BridgeDB, a Tor bridge distribution system.
